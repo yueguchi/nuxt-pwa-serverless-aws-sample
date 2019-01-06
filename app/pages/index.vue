60b9f7255f4fdc26ca8aaf2dcb0c246e7502ab2d
eguchi <eguchi@asial.co.jp>
2019-01-06 18:25:49 +0900

    pwa追加

---
+++ app/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <div>
+      <logo/>
+      <span>TOP</span>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style></style>
